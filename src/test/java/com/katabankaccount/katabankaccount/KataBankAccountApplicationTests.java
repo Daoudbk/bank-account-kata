593dbb9567c6ccd982c59699beb374dede6b571d
dwd <91daoud@gmail.com>
2020-11-25 12:22:15 +0100

    fist commit

---
+++ src/test/java/com/katabankaccount/katabankaccount/KataBankAccountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.katabankaccount.katabankaccount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KataBankAccountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
